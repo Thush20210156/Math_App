50e4f430ffe80509624c170900830aa285ee697a
Thushini Abeysuriya <thushiniabeysuriya@gmail.com>
2024-10-09 17:06:22 +0530

    Initial Commit

---
+++ Math_App/Math_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_AppApp.swift
+//  Math_App
+//
+//  Created by Thushini Abeysuriya on 2024-10-09.
+//
+
+import SwiftUI
+
+@main
+struct Math_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
